901b6e441e724aec8b02e7fff2c52f389e9a523d
vivekcse123 <vermavivek0402@gmail.com>
2025-01-03 00:24:05 +0530

    last commit

--- src/app/api/json-server/index.js
+++ src/app/api/json-server/index.js
@@ -1,11 +1,17 @@
-// /api/json-server/index.js
 const jsonServer = require('json-server');
 const path = require('path');
+
+// Create server
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, '../../angular-project/src/assets/db.json'));
+
+// Correct path to your db.json file for Vercel
+const router = jsonServer.router(path.join(__dirname, 'db.json')); // Place db.json in the same directory as this file or use a relative path
+
+// Use default middlewares (logger, static, cors, etc.)
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
+// Export as a Vercel serverless function
 module.exports = server;
